f7a15f30a05e0a17822b652534a0cb0083530aff
Harin Kim <harin14@naver.com>
2022-04-10 13:16:00 +0900

    feat: personal page

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,15 +2,41 @@ import { connect } from '../database'
 
 export const getUser = async(req, res) => {
     const connection = await connect();
+    const [userbadge] = await connection.query('SELECT badgeId FROM badgeintermediate WHERE userId = ?',[
+        req.params.id
+    ]);
+    const badgeArray = []
+    userbadge.forEach(async (content) => {
+        const [temp] = await connection.query('SELECT description FROM badge WHERE badgeId = ?',[
+            content.badgeId
+        ])
+        badgeArray.push(temp)
+    })
+
+    const challengeArray = []
+    const [challenged] = await connection.query('SELECT challengeId FROM challengeintermediate WHERE userId = ?',[
+        req.params.id
+    ]);
+    challenged.forEach(async (detail) => {
+        const [temp2] = await connection.query('SELECT title FROM challenge WHERE challengeId = ?',[
+            detail.challengeId
+        ])
+        const l1 = challengeArray.push(temp2)
+    })
+    
     const [usertable] = await connection.query('SELECT nickname,exp,tierId,totalCommit,flowerId FROM user WHERE userId = ?',[
         req.params.id
     ]);
+
     res.json({
         nickname: usertable[0].nickname,
         exp:usertable[0].exp,
         tierId: usertable[0].tierId,
         totalCommit: usertable[0].totalCommit,
         flowerId: usertable[0].flowerId,
+        badgeId: userbadge,
+        badge: badgeArray,
+        challenge: challengeArray,
     })
 }
 
